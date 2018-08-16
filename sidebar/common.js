c61ef4eae64673d02416b03410f3842e62921930
web-leozet <listikweb@gmail.com>
2018-08-16 17:18:18 +0300

    added width button

--- sidebar/common.js
+++ sidebar/common.js
@@ -1,6 +1,23 @@
+
+
 var button = document.querySelector('.btn');
 var sidebar = document.querySelector('.sidebar');
 
 button.addEventListener('click', function (e) {
 	sidebar.classList.toggle('is-hidden')
+	button.classList.toggle('on')
 }, false);
+
+
+/*var button = document.querySelector('.btn');
+var sidebar = document.querySelector('.sidebar');
+
+button.onclick = function () {
+	console.log(sidebar);
+	sidebar.classList.toggle('is-hidden')
+};*/
+
+var linkOne = document.querySelector('.one');
+linkOne.onclick = function () {
+	alert('Link 1');
+}
