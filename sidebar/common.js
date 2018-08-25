96e0453aab5b753773e91970264ebc752c91d438
web-leozet <listikweb@gmail.com>
2018-08-25 11:37:50 +0300

    folder_alarm

--- sidebar/common.js
+++ sidebar/common.js
@@ -2,10 +2,10 @@
 
 var button = document.querySelector('.btn');
 var sidebar = document.querySelector('.sidebar');
+console.log(sidebar);
 
 button.addEventListener('click', function (e) {
 	sidebar.classList.toggle('is-hidden')
-	button.classList.toggle('on')
 }, false);
 
 
