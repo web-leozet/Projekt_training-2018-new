19fda5be055dee2b99e83c1a1b51cf282558e8f3
web-leozet <listikweb@gmail.com>
2018-09-07 12:55:54 +0300

    slick_slider-test

---
+++ slider/slider.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+	$('.one-time').slick({
+		dots: true,
+		infinite: true,
+		autoplay: true,
+		autoplaySpeed: 1000,
+		slidesToShow: 3,
+		adaptiveHeight: true
+	});
+}
